c1044d40d1b727cc51d687a7af0d6b3d4f0116b1
junior231 <43250479+junior231@users.noreply.github.com>
2019-10-03 17:40:59 -0400

    packages installed, folders set up

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path'); // path lets us navigate file system / folders
+const hbs = require('hbs');
+
+// heroku assigns a port wheni it deploys via the process (environment variables - comin)
+// locally this will run @ port 3000; remotely it'll run wherever heroku tells it to run
+const port = process.env.PORT || 3000; // a double pipe means "or"
+
+const app = express();
+
+app.use(express.static('public'));
+
+// tell express to use the handlebars engine to render data
+app.set('view engine', 'hbs');
+
+// tell express to use the views folder to find its templates
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+  console.log('at the home route');
+  // res.sendFile(path.join(__dirname + '/views/index.html'));
+
+  res.render('home');
+  // this builds localhost:3000/views/index.html
+})
+
+app.get('/merch', (req, res) => {
+  console.log('at the merch route');
+  //res.sendFile(path.join(__dirname + '/views/contact.html'));
+  // this builds localhost:3000/views/contact.html
+  res.render('merch');
+})
+
+
+app.listen(port,  () => {
+  console.log(`Server running at ${port}/`);
+});
